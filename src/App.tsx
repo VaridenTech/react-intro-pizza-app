9b5e7921a81ecc0a9ecced1e53c56663166dccd0
Varis Darasirikul <manjarb4@gmail.com>
2026-07-30 01:31:41 +0700

    Refactor App component to remove Pizza and introduce Order component for creating pizza orders; add PizzaSize type definition.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // delete the React import
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 // delete the Pizza component
 
@@ -8,17 +8,7 @@ const App = () => {
   return (
     <div>
       <h1>Pizza app – Order Now</h1>
-      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" image={"/public/pizzas/pepperoni.webp"}/>
-      <Pizza
-        name="The Hawaiian Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="The Big Meat Pizza"
-        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
     </div>
   );
 };
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type PizzaSize = "S" | "M" | "L";
